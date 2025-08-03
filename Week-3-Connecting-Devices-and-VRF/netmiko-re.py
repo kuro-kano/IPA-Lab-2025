8451649102094bbb4650c481b98d9f6888a481f7
Gie.kaano <66070091@kmitl.ac.th>
2025-08-03 23:32:11 +0700

    add: get_uptime() function

--- Week-3-Connecting-Devices-and-VRF/netmiko-re.py
+++ Week-3-Connecting-Devices-and-VRF/netmiko-re.py
@@ -5,16 +5,34 @@ import re
 USERNAME = 'admin'
 PASSWORD = 'cisco'
 
+devices = [
+    {
+        'name': 'R1',
+        'ip': '172.31.16.4',
+        'device_type': 'cisco_ios',
+        'username': USERNAME,
+        'password': PASSWORD,
+    },
+    {
+        'name': 'R2',
+        'ip': '172.31.16.5',
+        'device_type': 'cisco_ios',
+        'username': USERNAME,
+        'password': PASSWORD,
+    }
+]
+
 def main():
   """Main function to configure devices using Netmiko with regex."""
   pass
 
-def get_active_interfaces(device_params):
+def get_active_interfaces(output):
   """Function to retrieve active interfaces using regex."""
   pass
 
-def get_uptime(device_params):
+def get_uptime(output):
   """Function to retrieve device uptime using regex."""
-  pass
+  pattern = r'uptime is (.*)'
+  match = re.search(pattern, output)
 
 main()
