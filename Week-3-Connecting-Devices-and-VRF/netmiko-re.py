7444317154beb19ede660dfb689bdbade5d47eef
Gie.kaano <66070091@kmitl.ac.th>
2025-08-03 22:52:20 +0700

    add: code structure

---
+++ Week-3-Connecting-Devices-and-VRF/netmiko-re.py
@@ -0,0 +1,20 @@
+"""netmiko-re.py"""
+from netmiko import ConnectHandler
+import re
+
+USERNAME = 'admin'
+PASSWORD = 'cisco'
+
+def main():
+  """Main function to configure devices using Netmiko with regex."""
+  pass
+
+def get_active_interfaces(device_params):
+  """Function to retrieve active interfaces using regex."""
+  pass
+
+def get_uptime(device_params):
+  """Function to retrieve device uptime using regex."""
+  pass
+
+main()
