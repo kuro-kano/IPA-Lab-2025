ced530d431a69fce895373ba5c018a031b5c0a09
Gie.kaano <66070091@kmitl.ac.th>
2025-08-03 22:11:25 +0700

    I was do so long then I notice wa I forgot to git checkout to netmiko-jinja sad na ;-; ok then this commit is add: correct jinja2

--- Week-3-Connecting-Devices-and-VRF/netmiko-jinja2.py
+++ Week-3-Connecting-Devices-and-VRF/netmiko-jinja2.py
@@ -1,40 +1,47 @@
 """netmiko-jinja2.py"""
 from netmiko import ConnectHandler
 from jinja2 import Environment, FileSystemLoader
-import yaml
+import yaml, os
 
 USERNAME = 'admin'
 PASSWORD = 'cisco'
 
 def main():
   """Main function to configure devices using Netmiko with Jinja2."""
-  # Load the device information from a YAML 
-  file_path = 'templates/device_info.yaml'
+  # Load the device information .YAML
+  script_dir = os.path.dirname(os.path.abspath(__file__))
+  file_path = os.path.join(script_dir, 'templates', 'devices_info.yaml')
 
-  with open(file_path, 'r') as file:
-    info = yaml.safe_load(file)
+  try:
+    with open(file_path, 'r') as file:
+      info = yaml.safe_load(file)
+  except FileNotFoundError:
+    print(f"Error: The file {file_path} does not exist.")
+    return
+  
+  template_dir = os.path.join(script_dir, 'templates')
+  env = Environment(loader=FileSystemLoader(template_dir))
+  template = env.get_template('devices_template.txt')
 
   for device in info:
-    configure_device(device)
+    configure_device(device, template)
 
-
-def configure_device(device_info):
+def configure_device(device, template):
   """Function to configure a device using Netmiko with Jinja2 template."""
 
-  env = Environment(loader=FileSystemLoader('templates'))
-  template = env.get_template('devices_template.txt')
-  
   device_params = {
-    'device_type': device_info['device_type'],
-    'ip': device_info['ip_address'],
+    'device_type': device['device_type'],
+    'ip': device['ip_address'],
     'username': USERNAME,
     'password': PASSWORD,
   }
 
   with ConnectHandler(**device_params) as ssh:
-    print(f"Connected to {device_info['name']}:{device_info['ip_address']}\nConfiguring...")
-    commands = template.render(device_info).strip()
+    print(f"Connected to {device['name']}:{device['ip_address']}\nConfiguring...")
+    commands = template.render(device).strip()
 
     result = ssh.send_config_set(commands.splitlines())
     print(result)
-    print(f"\n{device_info['ip']} configuration completed.\n----------\n")
+    print(f"\n{device['ip_address']} configuration completed.\n----------\n")
+
+main()
