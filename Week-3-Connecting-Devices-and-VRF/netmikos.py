06f753ff16e71f74304cf4aba6e9e9833f2ec93d
Gie.kaano <66070091@kmitl.ac.th>
2025-08-03 17:30:21 +0700

    add: configuration for R1 (CiscoIOSv-2 -- 172.31.16.4)

--- Week-3-Connecting-Devices-and-VRF/netmikos.py
+++ Week-3-Connecting-Devices-and-VRF/netmikos.py
@@ -6,17 +6,45 @@ PASSWORD = 'cisco'
 
 def main():
   """Main function to configure devices using Netmiko."""
-  configure_devices()
-
-def configure_devices():
-  """Function to configure devices using Netmiko."""
   configure_r1()
-  configure_r2()
-  configure_s1()
+  # configure_r2()
+  # configure_s1()
+
+def configure_devices(device_params, commands, device_name):
+  """Function to configure devices using Netmiko."""
+  with ConnectHandler(**device_params) as ssh:
+    print(f"Connected {device_name}\nConfiguring {device_name}...")
+    result = ssh.send_config_set(commands)
+    print(result)
+    print(f"{device_name} configuration completed.")
+
+  
 
 def configure_r1():
-  """Function to configure R1 (CiscoIOSv-2, 172.31.16.4) using Netmiko."""
-  pass
+  """
+  Function to configure R1 (CiscoIOSv-2, 172.31.16.4) using Netmiko.
+  Configures OSPF and loopback interface.
+  """
+  ip_address = '172.31.16.4'
+  
+  device_params = {
+    'device_type': 'cisco_ios',
+    'ip': ip_address,
+    'username': USERNAME,
+    'password': PASSWORD,
+  }
+
+  commands = [
+    'router ospf 1',
+    'router-id 1.1.1.1',
+    'network 10.0.0.0 0.0.0.255 area 0', # R1 -> R2
+    'int lo0',
+    'ip add 10.255.1.1 255.255.255.255', # loopback interface
+    'no shut',
+    'ip ospf 1 area 0',
+    ]
+
+  configure_devices(device_params, commands, "R1")
 
 def configure_r2():
   """Function to configure R2 (CiscoIOSv-2, 172.31.16.5) using Netmiko."""
@@ -41,17 +69,13 @@ def configure_s1():
     'name VLAN101',
     'exit',
     'interface vlan 101',
-    'ip address 10.0.101.1 255.255.255.0',
+    'ip address 10.0.101.2 255.255.255.0',
     'interface range g0/1-2',
     'switchport mode access',
     'switchport access vlan 101',
     'no shutdown',
   ]
-  
-  with ConnectHandler(**device_params) as ssh:
-    print("Connected S1\nConfiguring S1...")
-    result = ssh.send_config_set(commands)
-    print(result)
-    print("S1 configuration completed.")
+
+  configure_devices(device_params, commands, "S1")
 
 main()
